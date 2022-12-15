a1dbda479d5d9dee6aba3cf68df4bc407b911276
aholzschu <113791445+aholzschu@users.noreply.github.com>
2022-12-15 14:55:01 -0500

    html changes

--- main.js
+++ main.js
@@ -11,13 +11,29 @@ const new_pokemon = async (name) => {
     const pokemonname = document.createElement('h1')
     pokemonname.innerText = data['name']
     pokeCard.append(pokemonname)
+
+    const pokemonability = document.createElement('h3')
+    pokemonability.innerText = data['abilities'][0]['ability']['name']
+    pokeCard.append (pokemonability)
+
+    const pokemonability2 = document.createElement('h3')
+    pokemonability2.innerText = data['abilities'][1]['ability']['name']
+    pokeCard.append (pokemonability2)
+
+    const pokemonattack = document.createElement('h3')
+    pokemonattack.innerText = data['stats'][1]['base_stat']
+    pokeCard.append (pokemonattack)
+
+    const pokemondefense = document.createElement('h3')
+    pokemondefense.innerText = data['stats'][2]['base_stat']
+    pokeCard.append (pokemondefense)
 }
 
-const form_data = document.querySelector('#form-data')
+const form_data = document.querySelector('form')
 form_data.addEventListener('submit', (event) => {
   event.preventDefault()
-  const pokemonname = document.querySelector('#pokemon-data').value
-  get_pokemon_data(pokemonname)
+  const pokemonname = document.querySelector('#poke-data').value
+  new_pokemon(pokemonname)
   
 })
 
